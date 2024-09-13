152b6ce34a2042512c7e9c40c1510310ce3518b9
Guilhermecintrasss <guilhermecintrass@gmail.com>
2024-09-13 14:59:29 -0400

    a

--- aulas/ordenacao/quick_sort.c
+++ aulas/ordenacao/quick_sort.c
@@ -10,6 +10,40 @@ void troca(int *a, int *b)
   *a = *b;
   *b = aux;
 }
+int mediana(int n1, int n2,int n3){
+  if(n1 > n2){
+    troca(n1,n2);
+  }
+  if(n2>n3){
+    troca(n2,n3);
+  }
+  if(n1>n2){
+    troca(n1,n2);
+  }
+  return n2;
+}
+int separa_aleatorio_mediana(int p, int r, int v[4])
+{
+int x, i, j,t,n1,n2,n3;
+t = r-p+1; // tamanho do vetor
+n1 = v[p+rand()%t];
+n2 = v[p+rand()%t];
+n3 = v[p+rand()%t];
+x = mediana(n1,n2,n3);
+i = p - 1;
+j = r + 1;
+while (i < j) {
+do {
+j--;
+} while (v[j] > x);
+do {
+i++;
+} while (v[i] < x);
+if (i < j)
+troca(&v[i], &v[j]);
+}
+return j;
+}
 int separa_aleatorio(int p, int r, int v[4])
 {
 int x, i, j,t;
@@ -56,7 +90,7 @@ int main()
     v[1] = 3;
     v[2] = 4;
     v[3] = 2;
-    j = separa_aleatorio(0,3,v);
+    j = separa_aleatorio_mediana(0,3,v);
     
     for(int i = 0; i<4 ; i++){
         printf("%d ",v[i]);
